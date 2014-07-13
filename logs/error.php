186c47fc9ce5a4760298c292e25ab27d9cc6fa1b
Dorian <dars_55@hotmail.com>
2014-07-13 12:48:51 -0430

    carpeta logs

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-05-29 02:13:37 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-05-29T02:13:37+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-05-29T02:13:45+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-06-03T16:46:27+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-06-11T17:29:45+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
---
+++ logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-12 01:18:04 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-12T01:18:04+00:00	INFO	update	Actualización inciciadoa por el usuario Antonio Millán (991). La versión antigua es la 3.3.0.
+2014-06-12T01:18:05+00:00	INFO	update	Descargando el archivo desde .
+2014-06-12T01:18:20+00:00	INFO	update	El archivoe Joomla_3.3.x_to_3.3.1-Stable-Patch_Package.zip se ha descargado correctamente.
+2014-06-12T01:18:20+00:00	INFO	update	Iniciando la instalación de la versión nueva.
+2014-06-12T01:18:25+00:00	INFO	update	Finalizando la instalación.
+2014-06-12T01:18:26+00:00	INFO	update	Borrando los archivos y carpetas que han sido eliminados.
+2014-06-12T01:18:43+00:00	INFO	update	Limpieza posterior a la instalación.
+2014-06-12T01:18:43+00:00	INFO	update	La actualización a la versión 3.3.1 se ha completado.
